47b54101da54f0c9fcccfb3b15dbd45bddff8e89
Leonardo <lfotecam@gmail.com>
2019-07-19 17:35:12 -0500

    Update project

---
+++ pages/channel.js
@@ -0,0 +1,7 @@
+export default class extends React.Component{
+    render(){
+        return (
+            <h1>Vista de canal</h1>
+        )
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import 'isomorphic-fetch';
+import Link from 'next/link';
 export default class extends React.Component{
     static async getInitialProps(){
         let request = await fetch('https://api.audioboom.com/channels/recommended')
@@ -9,20 +10,57 @@ export default class extends React.Component{
         const { channels } = this.props
         return (
            <div>
-                <header>Podcast</header>
+                <header>
+                    Este no es un podcast normal
+                </header>
+                <div className='channels'>
                 {
                     channels.map(channel => (
-                        <div class='channel'>
-                            <img src={channel.urls.logo_image.original} alt={channel.title}/>
-                        </div>
+                        <Link href='/channel' key={channel.id} prefetch>
+                            <a className='channel'>
+                                <img src={channel.urls.logo_image.original} alt={channel.title}/>
+                                <h2>{channel.title}</h2>
+                            </a>
+                        </Link>
                     ) )
                 }
+                </div>
                 <style jsx>{`
                     header{
                         color: #fff;
                         background: #8756ca;
                         padding: 15px;
                         text-align: center;
+                    }
+                    .channels{
+                        display: grid;
+                        grid-gap: 15px;
+                        padding: 15px;
+                        grid-template-columns: repeat(auto-fill,minmax(160px,1fr));
+                    }
+                    .channel {
+                        display: block;
+                        border-radius: 3px;
+                        box-shadow: 0px 2px 6px rgb(0,0,0,0.15);
+                        margin-bottom: 0.5rem;
+                    }
+                    .channel h2{
+                        color: #000;
+                        text-decoration: none !important;
+                    }
+                    .channel h2:hover{
+                        color: #000;
+                        
+                    }
+                    .channel img{
+                        width: 100%;
+                    }
+                    .channel h2{
+                        padding: 5px;
+                        font-size: 0.9rem;
+                        font-weight: bold;
+                        margin: 0;
+                        text-align:center;
                     }
                 `}</style>
                 <style jsx global>{`
